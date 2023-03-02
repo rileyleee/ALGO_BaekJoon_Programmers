60cc95b3c2348a6cad2b1974c78149a76f39cfda
rileyleee <116135258+rileyleee@users.noreply.github.com>
2023-03-02 23:09:21 +0900

    [D3] Title: 농작물 수확하기, Time: 178 ms, Memory: 24,500 KB -BaekjoonHub

---
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int test = sc.nextInt();
+		for (int t = 1; t <= test; t++) {
+			int N = sc.nextInt();
+			int sum = 0;
+			for (int i = 0; i < N; i++) {
+				String str = sc.next();
+				for (int j = 0; j < N; j++) {
+					int temp = str.charAt(j) - 48;
+					if (Math.abs(N / 2 - i) + Math.abs(N / 2 - j) <= N / 2) {
+						sum += temp;
+					}
+				}
+			}
+			System.out.println("#" + t + " " + sum);
+		}
+		sc.close();
+	}
+}
