5347e32e921ca3bd598e718a8f298dd6c26f50b5
rileyleee <116135258+rileyleee@users.noreply.github.com>
2023-03-04 10:00:16 +0900

    [Silver III] Title: 수열, Time: 1884 ms, Memory: 93600 KB -BaekjoonHub

---
+++ 백준/Silver/2559. 수열/수열.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+
+		int[] arr = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		int sum = 0;
+		int max = Integer.MIN_VALUE;
+		for (int i = 0; i < n - k + 1; i++) {
+			sum = 0;
+			for (int j = i; j < k + i; j++) {
+				sum += arr[j];
+			}
+			max = Math.max(max, sum);
+		}
+		System.out.println(max);
+		sc.close();
+	}
+}
