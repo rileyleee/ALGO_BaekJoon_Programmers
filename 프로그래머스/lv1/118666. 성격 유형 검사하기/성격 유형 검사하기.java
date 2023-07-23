4d9c7688ce2a8280ac51996e12e4ec657c70467c
Lee EunGyeong <116135258+rileyleee@users.noreply.github.com>
2023-07-23 09:42:19 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.27 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -0,0 +1,69 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] survey, int[] choices) {
+        
+        // 각 지표별 배열 
+        int [] type = new int[21];
+        
+        int sLength = survey.length;
+        
+        for(int i = 0; i < sLength; i++){
+            int response = choices[i];
+            int fl = survey[i].substring(0,1).charAt(0)-64;
+            int sl = survey[i].substring(1).charAt(0)-64;
+           
+            if(response<4){
+                switch (response) {
+                    case 1: type[fl]+=3;
+                    break;
+                    case 2: type[fl]+=2;
+                    break;
+                    case 3: type[fl]+=1;
+                    break;
+                }              
+            }else if(response>4){
+                type[sl]+=response-4;
+            }
+            
+
+              // System.out.println(fl);
+              // System.out.println(sl);
+
+
+             // 18 20, 3 6, 10 13, 1 14
+
+            
+            }
+        
+        StringBuilder sb = new StringBuilder();
+        // int R,
+        // for(int i = 0; i<type.length; i++) {
+        //     int R = type[18]; int T = type[20]; int C = type[3]; int F = type[6];
+        //     int J = type[10]; int M = type[13]; int A = type[1]; int N = type[14];            
+        // }
+        
+        if(type[18]>=type[20]) sb.append("R");
+        else sb.append("T");
+        
+        if(type[3]>=type[6]) sb.append("C");
+        else sb.append("F");
+        
+        if(type[10]>=type[13]) sb.append("J");
+        else sb.append("M");
+        
+        if(type[1]>=type[14]) sb.append("A");
+        else sb.append("N");
+            
+            
+        
+        
+        
+        
+        
+        
+        String answer = sb.toString();
+        
+        return answer;
+    }
+}
