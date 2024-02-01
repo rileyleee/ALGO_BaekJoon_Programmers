05c6b05af93690f09750a3c4eb95d36291387826
Lee EunGyeong <116135258+rileyleee@users.noreply.github.com>
2024-02-01 15:29:41 +0900

    [Silver IV] Title: ATM, Time: 164 ms, Memory: 16204 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -2,24 +2,30 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int p = sc.nextInt();
-		int[] time = new int[p];
-		for (int t = 0; t < p; t++) {
-			time[t] = sc.nextInt();
-		}
-		Arrays.sort(time);
-		int[] timeSum = new int[p];
-		timeSum[0] = time[0];
-		for (int ts = 1; ts < p; ts++) {
-			timeSum[ts] = timeSum[ts - 1] + time[ts];
-		}
-		int sum = 0;
-		for (int i = 0; i < p; i++) {
-			sum += timeSum[i];
-		}
-		System.out.println(sum);
-		sc.close();
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        for(int i =0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        int [] time = new int[n];
+
+
+        time[0]  = arr[0];
+
+        long answer = time[0];
+
+        for(int i =1; i<n; i++){
+            time[i] = time[i-1]+arr[i];
+            answer +=time[i];
+        }
+
+        System.out.println(answer);
+    }
 }
