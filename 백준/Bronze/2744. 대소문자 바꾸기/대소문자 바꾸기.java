a32d4b300713840cb1c2eceb1a7cee66e260a1b1
Lee EunGyeong <116135258+rileyleee@users.noreply.github.com>
2024-02-10 00:52:07 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 104 ms, Memory: 12764 KB -BaekjoonHub

--- 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
@@ -6,25 +6,26 @@ public class Main {
     public static void main(String[] args) {
         input();
         solve();
-
-
     }
 
     private static void solve() {
 
-        String upperS = targetStr.toUpperCase();
-        String lowerS = targetStr.toLowerCase();
+        String ans = "";
 
-        StringBuilder sb = new StringBuilder();
 
-        for(int i =0; i<targetStr.length(); i++){
-            if(targetStr.charAt(i)-0>96){
-                sb.append(upperS.charAt(i));
-            }else{
-                sb.append(lowerS.charAt(i));
+        for (int i = 0; i < targetStr.length(); i++) {
+            if ('A' <= targetStr.charAt(i) && targetStr.charAt(i) <= 'Z') {
+                // 각 더하기 연산은 char를 아스키 코드로 변환하여 계산하기 때문에
+                // 대문자일 경우 A로부터 얼마나 거리가 떨어져 있는지 차이를 구하고
+                // 소문자 a로부터 떨어진 거리를 더해주면 해당 아스키 코드를 가진 소문자가 된다
+                // 이를 다시 char로 캐스팅하면 문자로 출력 가능
+                ans += (char) ('a' + targetStr.charAt(i) - 'A');
+            } else {
+                ans += (char) ('A' + targetStr.charAt(i) - 'a');
             }
         }
-        System.out.println(sb.toString());
+
+        System.out.println(ans);
 
     }
 
