a12e7ac9a103bd5e6c22c86012c000e8abe36d3d
Lee EunGyeong <116135258+rileyleee@users.noreply.github.com>
2024-02-11 22:42:55 +0900

    [Silver V] Title: 문서 검색, Time: 116 ms, Memory: 13260 KB -BaekjoonHub

--- 백준/Silver/1543. 문서 검색/문서 검색.java
+++ 백준/Silver/1543. 문서 검색/문서 검색.java
@@ -11,21 +11,50 @@ public class Main {
     }
 
     private static void solve() {
-        //System.out.println("변경 전 문서: " + document);
-
-        document = document.replace(target, "*");
-
+        int startIdx = 0;
+        int dIdx = 0;
+        int tIdx = 0;
         int ans = 0;
 
-        for (int i = 0; i < document.length(); i++) {
-            if (document.charAt(i) == '*') {
-                //System.out.println("찾았어요");
-                ans++;
+        while (dIdx < document.length()) {
+
+            //System.out.println("0) dIdx: " + dIdx);
+            //System.out.println("0) tIdx: " + tIdx);
+
+            if (document.charAt(dIdx) == target.charAt(tIdx)) {
+                tIdx++;
+                //System.out.println("1) dIdx: " + dIdx);
+                //System.out.println("1) tIdx: " + tIdx);
+                if (tIdx == target.length()) {
+                    tIdx = 0;
+                    ans++;
+                    startIdx = dIdx;
+                    //System.out.println("2) dIdx: " + dIdx);
+                    //System.out.println("2) tIdx: " + tIdx);
+                }
+                dIdx++;
+                //System.out.println("3) dIdx: " + dIdx);
+                //System.out.println("3) tIdx: " + tIdx);
+            } else {
+                if (tIdx == 0) {
+
+                    dIdx++;
+                    startIdx = dIdx;
+                    //System.out.println("4) dIdx: " + dIdx);
+                    //System.out.println("4) tIdx: " + tIdx);
+                } else {
+                    tIdx = 0;
+                    startIdx++;
+                    dIdx = startIdx;
+                    //System.out.println("5) dIdx: " + dIdx);
+                    //System.out.println("5) tIdx: " + tIdx);
+                }
             }
+
         }
-        //System.out.println("변경된 문서: " + document);
-        //System.out.println("대상 단어: " + target);
+
         System.out.println(ans);
+
 
     }
 
