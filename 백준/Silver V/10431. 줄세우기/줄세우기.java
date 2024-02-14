7271df0a4b3aacb1750593e1fd87959edf4bf250
Lee EunGyeong <116135258+rileyleee@users.noreply.github.com>
2024-02-14 20:38:19 +0900

    [Silver V] Title: 줄세우기, Time: 412 ms, Memory: 35284 KB -BaekjoonHub

--- 백준/Silver V/10431. 줄세우기/줄세우기.java
+++ 백준/Silver V/10431. 줄세우기/줄세우기.java
@@ -27,32 +27,20 @@ public class Main {
                 thisT = sc.nextInt();
             } else {
                 height[i] = sc.nextInt();
-                howMany += reordering(i);
             }
         }
-        System.out.println(thisT + " " + howMany);
-    }
-
-    private static int reordering(int idx) {
-        int targetNum = height[idx];
-        int thisMove = 0;
 
         for (int i = 0; i < 20; i++) {
-            if (height[i] != 0 && height[i] > height[idx]) {
-//                System.out.println("height[i]: " + height[i]);
-//                System.out.println("height[idx]: " + height[idx]);
-//                System.out.println("=========================================");
-                for (int j = 19; j > i; j--) {
-
-                    if (height[j] != 0) {
-                        height[j] = height[j - 1];
-                        thisMove++;
-                    }
+            for (int j = 0; j < i; j++) {
+                if (height[i] < height[j]) {
+                    howMany++;
                 }
-                height[i] = targetNum;
-//                System.out.println("이번 이동 수: " + thisMove);
             }
         }
-        return thisMove;
+
+        System.out.println(thisT+" "+howMany);
+
     }
+
+
 }
