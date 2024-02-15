bf4900a2fa70174fbae4366e8d03b0d04ca86115
Lee EunGyeong <116135258+rileyleee@users.noreply.github.com>
2024-02-15 16:12:03 +0900

    [Silver III] Title: 두 수의 합, Time: 708 ms, Memory: 92372 KB -BaekjoonHub

--- 백준/Silver III/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver III/3273. 두 수의 합/두 수의 합.java
@@ -1,41 +1,46 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static int n, x;
+    public static boolean[] exist;
+
+    public static void main(String[] args) { // 카운팅 배열을 이용한 풀이
+        input();
+        solve();
+    }
+
+    private static void input() {
         Scanner sc = new Scanner(System.in);
-        int len = sc.nextInt();
-        int[] arr = new int[len];
 
-        for(int i =0; i<len; i++){
-            arr[i] = sc.nextInt();
+        n = sc.nextInt();
+        exist = new boolean[1000001];
+
+        // 존재하면 true로 변경
+        for (int i = 0; i < n; i++) {
+            exist[sc.nextInt()] = true;
         }
 
-        int num = sc.nextInt();
 
-        long answer = 0;
+        x = sc.nextInt();
+    }
 
-        Arrays.sort(arr);
+    private static void solve() {
 
-        int a = 0;
-        int b = len-1;
+        int ans = 0;
 
-        while(a<=b){
+        // 1부터 절반까지 순회하면서
+        // 기준 숫자와 x-기준숫자가 둘다 존재하면 ans에 값을 더해줌
+        // 이 때 x/2가 아니라 (x-1)/2라고 하는 이유는 x가 짝수일 때 같은 수로 합을 만드는 경우를 배제하기 위함임
 
-            if(a==b){
-                break;
-            }
+        for (int i = 1; i <= (x - 1) / 2; i++) {
 
-            if(arr[a]+arr[b]==num){
-                a++;
-                b--;
-                answer++;
-            }else if(arr[a]+arr[b]>num){
-                b--;
-            }else if(arr[a]+arr[b]<num){
-                a++;
+            if (i <= 1000000 && x - i <= 1000000) {
+                ans += (exist[i] && exist[x - i]) ? 1 : 0;
             }
         }
-        System.out.println(answer);
+
+
+        System.out.println(ans);
+
     }
 }
