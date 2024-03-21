d42c42e7cdfe5e57ed0a5b7b7aa0b243c9f08ed0
Lee EunGyeong <116135258+rileyleee@users.noreply.github.com>
2024-03-21 22:29:32 +0900

    [Silver IV] Title: 수들의 합 2, Time: 116 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -16,7 +15,6 @@ public class Main {
         int m = Integer.parseInt(st.nextToken());
 
         int[] arr = new int[n + 1];
-        int[] acc = new int[n + 1];
 
         st = new StringTokenizer(br.readLine());
 
@@ -25,30 +23,23 @@ public class Main {
         }
         //System.out.println(Arrays.toString(arr));
 
-        acc[1] = arr[1];
-        for (int i = 2; i <= n; i++) {
-            acc[i] = acc[i - 1] + arr[i];
-        }
-        //System.out.println(Arrays.toString(acc));
 
         int cnt = 0;
 
-        int l = 1;
-        int r = 1;
+        int rightIdx = 1;
 
-        while (l <= n && r <= n) {
-            //System.out.printf("l은 %d, r은 %d", l, r);
-            //System.out.println();
-            if (acc[r] - acc[l - 1] == m) {
-                r++;
-                cnt++;
-                //System.out.println("더해졌어요");
-            } else if (acc[r] - acc[l - 1] > m) l++;
-            else r++;
+        int currentSum = arr[1];
+
+        for (int i = 1; i <= n; i++) {
+            while (currentSum < m && rightIdx < n) {
+                currentSum += arr[++rightIdx];
+            }
+            if (currentSum == m) cnt++;
+            // 만약 현재까지 더한 값이 m과 같은 게 아니라 컸다면 이 시작점에서는 m을 만들 구간이 없다는 것을 의미하니까
+            // 시작점의 값을 뺀 기존의 합인 값이 m과 비교해서 작은지 큰지 또 비교한다.
+            currentSum -= arr[i];
         }
-
         System.out.println(cnt);
-
-
     }
+
 }
