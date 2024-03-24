e2ab3a7d25b76332145398fc95aafaf292db2787
Lee EunGyeong <116135258+rileyleee@users.noreply.github.com>
2024-03-24 21:02:01 +0900

    [level 1] Title: 모의고사, Time: 0.52 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -6,24 +6,28 @@ class Solution {
         int [] st2 = {2,1,2,3,2,4,2,5};        
         int [] st3 = {3,3,1,1,2,2,4,4,5,5};
         
-        int [] cnt = new int [3];
+        int cor1 = 0;
+        int cor2 = 0;
+        int cor3 = 0;
+        
         
         for(int i =0; i<answers.length; i++){
             
-            if(answers[i] == st1[i % 5]) cnt[0]++;
-            if(answers[i] == st2[i % 8]) cnt[1]++;
-            if(answers[i] == st3[i % 10]) cnt[2]++;
+            if(answers[i] == st1[i % 5]) cor1++;
+            if(answers[i] == st2[i % 8]) cor2++;
+            if(answers[i] == st3[i % 10]) cor3++;
             
         }
         
-        int highest = Math.max(cnt[0], Math.max(cnt[1], cnt[2]));
+        int highest = Math.max(cor1, Math.max(cor2, cor3));
         
         List<Integer> list = new ArrayList<>();
         
-        for(int i = 0; i < 3; i++) {
-            if(cnt[i] == highest) list.add(i+1);
-        }       
         
+        if(cor1 == highest) list.add(1);
+        if(cor2 == highest) list.add(2);
+        if(cor3 == highest) list.add(3);      
+               
         int [] answer = new int [list.size()];
         
         for(int i =0; i<list.size(); i++) answer[i] = list.get(i);
